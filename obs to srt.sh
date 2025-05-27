2580eceef335898f7abe0466a21608983bc232dd
sphan007 <32421041+sphan007@users.noreply.github.com>
2025-05-27 14:12:15 -0700

    Added comment lines

--- obs to srt.sh
+++ obs to srt.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 ffmpeg -i "srt://127.0.0.1:9999?mode=caller" \
+  # set fps and resolution
   -vf "fps=59.94,scale=1920:1080" \
+  # set bitrate
   -c:v libx264 -b:v 10M -minrate 5M -maxrate 5M -bufsize 20M -x264opts nal-hrd=cbr -r 59.94 -pix_fmt yuv420p -profile:v high -level 4.1 \
   -c:a aac -b:a 192k -ac 2 -ar 48000 -sample_fmt fltp \
+  # set pids
   -map 0:v:0 -map 0:a:0 \
   -streamid 0:260 -streamid 1:250 \
   -mpegts_service_type digital_tv \
@@ -11,5 +14,6 @@ ffmpeg -i "srt://127.0.0.1:9999?mode=caller" \
   -mpegts_pmt_start_pid 32 \
   -mpegts_start_pid 260 \
   -mpegts_copyts 1 \
+  # set srt endpoints
   -f mpegts \
   "srt://ip_address:port_number?pkt_size=1316&mode=caller"
