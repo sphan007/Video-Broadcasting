02f1660ce2406f7296e4186f8b30033d607e69f6
sphan007 <32421041+sphan007@users.noreply.github.com>
2025-05-27 14:14:14 -0700

    added comments

--- videofile_to_srt.sh
+++ videofile_to_srt.sh
@@ -3,6 +3,7 @@ ffmpeg -re -stream_loop -1 -i "path_to_video_file" \
   -vf "fps=59.94,scale=1920:1080" \
   -c:v libx264 -b:v 10M -minrate 5M -maxrate 5M -bufsize 20M -x264opts nal-hrd=cbr -r 59.94 -pix_fmt yuv420p -profile:v high -level 4.1 \
   -c:a aac -b:a 192k -ac 2 -ar 48000 -sample_fmt fltp \
+  # set PIDs for SRT. 260 for video 250 for audio
   -map 0:v:0 -map 0:a:0 \
   -streamid 0:260 -streamid 1:250 \
   -mpegts_service_type digital_tv \
